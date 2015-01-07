6ba63613d2dacc2d9d7d266f68f67e73dbd4f7a1
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-07 11:49:29 -0800

    Signed-off-by: GuillermoReyes <greyes29@student.rcc.edu>

---
+++ Class_Lab/math instructor disagreement v2/main.cpp
@@ -0,0 +1,34 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Reyes, Guillermo
+ * Purpose: how easy computers can mistakes
+ * Created on January 7, 2015, 11:17 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// System Libraries
+// User libraries
+// Function prototypes
+int main(int argc, char** argv) {
+    // Declare 3 variables 
+    float a,b,c;
+    //Prompt for a and b
+    
+    cout<<"Input 2 float values"<<endl;
+    cin>>a>>b;
+    
+    //Sum the values a + b
+    c=a+b;
+    
+    //output the results
+    cout<<c<<"="<<a<<"  "<<b<<endl;
+    //Exit stage right!
+    
+    return 0;
+    
+    
+}
