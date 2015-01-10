a664a3bb16a4de1adea91f2113b34ac3d3e0a82f
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-10 09:26:43 -0800

    Initial Commit

---
+++ Hmwk/Savitch9thEd_hapter1_practProb5/Savitch9thEd_hapter1_practProb5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Savitch9thEd_hapter1_practProb5
+//
+//  Created by Guillermo  on 1/10/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
