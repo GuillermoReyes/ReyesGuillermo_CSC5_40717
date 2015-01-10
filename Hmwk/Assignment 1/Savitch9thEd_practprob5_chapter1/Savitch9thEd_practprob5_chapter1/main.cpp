9fea20a60d8d6ae09343ac58e4fb9a4d3483eb6d
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-10 09:31:36 -0800

    New problem added
    Problem replaced by another one

--- Hmwk/Assignment 1/Savitch9thEd_practprob5_chapter1/Savitch9thEd_practprob5_chapter1/main.cpp
+++ Hmwk/Assignment 1/Savitch9thEd_practprob5_chapter1/Savitch9thEd_practprob5_chapter1/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+//This program is a modifiation so that we may calculate the area of a rectangular space
+int main( )
+{
+    // Introduce the variables
+    float width, length, area;
+    
+    //Prompt user for width
+    cout << "Enter the width:\n";
+    cin >> width;
+    
+    //Prompt for length
+    cout << "Enter the length:\n";
+    cin >> length;
+    
+    area = (2*width)+(2*length);
+    
+    cout << "If you have a length of  ";
+    cout << length;
+    
+    cout << " and a width of ";
+    cout << width;
+    
+    //Execute alculation
+    cout << " you have an area of: ";
+    cout << area;
+    cout<<"  \n";
+    
     return 0;
 }
