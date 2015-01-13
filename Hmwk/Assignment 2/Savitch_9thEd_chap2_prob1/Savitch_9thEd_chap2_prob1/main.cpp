fcc4c56cbaca6d5c5b394155109ab15ac2c5fcc1
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-12 19:07:42 -0800

    Initial Commit

--- Hmwk/Assignment 2/Savitch_9thEd_chap2_prob1/Savitch_9thEd_chap2_prob1/main.cpp
+++ Hmwk/Assignment 2/Savitch_9thEd_chap2_prob1/Savitch_9thEd_chap2_prob1/main.cpp
@@ -8,8 +8,29 @@
 
 #include <iostream>
 
+using namespace std;
+
+float const SIS    =      350*.01; //How many grams per soda can that are the sweetener
+float const DTHRAT =     .1429;    // The death rati0; percentage to find lethal dose of sweetener
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    float Weight; //weight of the user
+    
+    cout<<" what is the ideal weight you would like to have, in grams?\n";
+    cout<<"at what weight will you stop dieting?)\n";
+    cin>>Weight;
+    
+    // This is the specific "death capacity" calculated for the specific user
+    float specdos = DTHRAT*Weight;
+    // How many can will kill the user
+    int death     = specdos/SIS;
+    
+    cout<<"You will die if you drink "<<death<<" cans of soda";
+    
+    
+    
+    
+    
     return 0;
 }
---
+++ Hmwk/Assignment 2/Savith_9thEd_chapter2_prob13/Savith_9thEd_chapter2_prob13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Savith_9thEd_chapter2_prob13
+//
+//  Created by Guillermo  on 1/12/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
