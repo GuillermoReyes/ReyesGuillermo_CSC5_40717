c6aea2aab4181542272f4bae1e10c9921a3fb7e9
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-13 15:58:41 -0800

    Initial Commit

--- Hmwk/Assignment 2/Savitch_9thEd_chapter2_prob6/Savitch_9thEd_chapter2_prob6/main.cpp
+++ Hmwk/Assignment 2/Savitch_9thEd_chapter2_prob6/Savitch_9thEd_chapter2_prob6/main.cpp
@@ -1,18 +1,15 @@
 //
 //  main.cpp
-//  savitch_9thEd_chapter2_prob14
+//  Savitch_9thEd_chapter2_prob6
 //
 //  Created by Guillermo  on 1/13/15.
 //  Copyright (c) 2015 Guillermo . All rights reserved.
 //
 
 #include <iostream>
-using namespace std;
-
 
 int main(int argc, const char * argv[]) {
-    
-    
-    
+    // insert code here...
+    std::cout << "Hello, World!\n";
     return 0;
 }
