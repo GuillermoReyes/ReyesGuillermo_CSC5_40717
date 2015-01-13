de53b000722b17bd722c274d7a45abb10d616141
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-13 15:14:12 -0800

    Initial Commit

---
+++ Hmwk/Assignment 2/savitch_9thEd_chapter2_prob14/savitch_9thEd_chapter2_prob14/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  savitch_9thEd_chapter2_prob14
+//
+//  Created by Guillermo  on 1/13/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
