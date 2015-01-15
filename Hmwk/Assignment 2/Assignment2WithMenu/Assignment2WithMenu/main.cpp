25004252428e94c4725fc882ec477f7b88e66a3e
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-14 18:53:38 -0800

    Initial Commit

---
+++ Hmwk/Assignment 2/Assignment2WithMenu/Assignment2WithMenu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2WithMenu
+//
+//  Created by Guillermo  on 1/14/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
