7e01c9a973cd0d05a27e65cec2d35825724e3ecd
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-19 11:35:30 -0800

    Problem 7 completed

--- Hmwk/Assignment 3/savitch_9thEd_chapter3_problem7/savitch_9thEd_chapter3_problem7/main.cpp
+++ Hmwk/Assignment 3/savitch_9thEd_chapter3_problem7/savitch_9thEd_chapter3_problem7/main.cpp
@@ -8,22 +8,50 @@
 
 #include <iostream>
 #include <cmath>
-
+#include <iomanip>
 using namespace std;
 
+
+int factorial(int n)
+{
+    if (n == 0) return 1;
+    if (n < 3) return n;
+    return n * factorial(n -1);
+}
 int main(int argc, const char * argv[]) {
     cout<<"e^x an be approximated using a taylor polynimial.Let's test how close to the actual value it can approximate.\n";
     cout<<" a taylor polynomial is just a series expansion that an be iterated 'n' times\n ";
+    cout<<endl;
     
-    int n; //The amount of iteration of the series
-    float x; //The value of x that we are trying to find
+    int s; //The amount of iteration of the series
+    int x; //The value of x that we are trying to find
     
-    cout<<"Choose a valuer of x that you would like to input:";
+    cout<<"Choose a value of x that you would like to input:";
     cin>>x;
-    
+    cout<<endl;
+    cout<<"The anticipated value of e is :"<<exp(x)<<endl;
     cout<<"Choose the number of iterations that you would like to perform on this function:";
- 
-        
+    cin>>s;
+    float sum;
     
+
+    
+    
+    for( int i=0;i<=s;i++){
+        
+        
+        float ex= pow(x,i)/factorial(i);
+        sum+=ex;
+        cout<<fixed<<showpoint<<setprecision(3);
+        cout<<"After "<<i<<" iterations the value is: "<<sum<<endl;
+       
+        
+        
+        
+    }
     return 0;
+ 
+    
+    
+    
 }
