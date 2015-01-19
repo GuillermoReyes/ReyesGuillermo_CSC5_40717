f23b494647c96807f1954cde33869bdbb7040015
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-18 18:20:19 -0800

    Problem 5 updated

--- Hmwk/Assignment 3/Savitch_9thEd_chapter3_problem5/Savitch_9thEd_chapter3_problem5/main.cpp
+++ Hmwk/Assignment 3/Savitch_9thEd_chapter3_problem5/Savitch_9thEd_chapter3_problem5/main.cpp
@@ -7,8 +7,58 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
+
+int main() {
+    float intpay,ibalance,install,i,period; //interest payment,loan amount,installment amount,interest rate,mo. to pay loan
+    
+    cout<<"What is the interest rate? (%):";
+    cin>>i;
+    cout<<"What is the loan amount? :";
+    cin>>ibalance;
+    cout<<"what is the term of the loan (in months) :";
+    cin>>period;
+    int month=0;
+    install=ibalance/period;
+    float balance=ibalance;         //shows that initial balance is equal to loan amount
+    float MonthInt= (i/100)/12;     //the monthly interest rate
+    float TotalInt=0;               //The sum of the Interest paid (for while loop)
+    float Princpay;                 //Amount paid towards principal
+    
+    
+    
+    
+    
+    
+    while(balance>install){
+        month++;                    //Counts the months
+        intpay=MonthInt*balance;    //money paid towards interest
+        TotalInt+=intpay;           //Sum of all Interest paid
+        Princpay= install-intpay;
+        balance-=Princpay;
+        cout<<fixed<<showpoint<<setprecision(2);
+        cout<<"at month "<<month<<" the interest paid is $"<<TotalInt<<endl;
+        cout<<" and the remaining balance is $"<<balance<<endl;
+    }
+        cout<<"At month "<<month<<" the balance is "<<balance<<"\n";
+        cout<<endl;
+        cout<<"The total amount paid towards interest is $"<<TotalInt<<endl;
+    
+    return 0;
+}
+
+        
+    
+    
+    
+    
+    
+
+
+
+
 
 
 
