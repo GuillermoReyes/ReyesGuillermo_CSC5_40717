87a94638a52b72811de186d1b8af300895db7da5
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-20 21:12:56 -0800

    New hw assignment

---
+++ Hmwk/Assignment 4/Savitch_9thEd_pracProb3/Savitch_9thEd_pracProb3/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Savitch_9thEd_pracProb3
+//  Stok price problem
+//  Created by Guillermo  on 1/20/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int amount; //amount of stoks owned
+    int whole; //Whole number amount of the stocks
+    int num;   //numerator of rational part
+    int den;   //denominator of fractional part
+    
+    cout<<"What is the amount of a specific stock that you own?";
+    cin>>amount;
+    
+
+    return 0;
+}
