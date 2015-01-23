777af3671d2ca53514110e0491b17c2f24cad47a
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-23 14:57:39 -0800

    Additions
    Added do-while loop and include some comments

--- Hmwk/Assignment 4/Savitch_9thEd_chapter4_problem7/Savitch_9thEd_chapter4_problem7/main.cpp
+++ Hmwk/Assignment 4/Savitch_9thEd_chapter4_problem7/Savitch_9thEd_chapter4_problem7/main.cpp
@@ -11,18 +11,20 @@
 using namespace std;
 
 //Function prototypes
-int computeDifference(int hr,int HR,int min,int MIN,char isAM){
+int computeDifference(int hr,int HR,int min,int MIN,char isAM){  //hr=initilal hour,HR= final time in minutes
     float Hours = HR-hr;
-    float Mins  =MIN-min;
-    if(isAM=='y'||isAM=='Y') return Hours*60+Mins;
+    float Mins  =MIN-min;                                        //min=initial min,MIN= end time in  minutes
+    if(isAM=='y'||isAM=='Y') return Hours*60+Mins;               //isAM=determine if they are in AM or PM
     if(isAM=='n'||isAM=='N') return Hours*60+12*60+Mins;
     return Hours*60 +Mins;
 }
 
 int main() {
-    int hr1,hr2,min1,min2;
-    char Equal;
-    
+    int hr1,hr2,min1,min2;  //initial hour,final hour,initial minute,  final minute
+    char Equal;             //Determines if it is AM or PM
+    char choose;            //ariable to prompt uder if they would like to continue
+    //Exeution Begins here
+    do {
     cout<<"Welcome to my time Machine!"<<endl;
     cout<<"You may only go forward up to 25 hours."<<endl;
     cout<<"Start time in hours : "<<endl;
@@ -38,6 +40,10 @@ int main() {
     cout<<"If incorrect input chosen, program will assume they are of same time period. "<<endl;
     cin>>Equal;
     cout<<"The amount travelled in minutes is : "<<computeDifference(hr1,hr2,min1,min2,Equal)<<endl;
+        cout<<"Would you like to try again? "<<endl;
+        cin>>choose;
+    } while(choose=='Y'||choose=='y');
+    //Exit Program. 
     
     return 0;
 }
