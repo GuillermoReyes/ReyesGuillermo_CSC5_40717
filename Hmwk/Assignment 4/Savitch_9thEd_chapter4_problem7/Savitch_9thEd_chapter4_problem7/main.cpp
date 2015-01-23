f178afc785520c3bc8af851b42e5fedb31232b56
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-23 14:52:07 -0800

    New problem added and completed

--- Hmwk/Assignment 4/Savitch_9thEd_chapter4_problem7/Savitch_9thEd_chapter4_problem7/main.cpp
+++ Hmwk/Assignment 4/Savitch_9thEd_chapter4_problem7/Savitch_9thEd_chapter4_problem7/main.cpp
@@ -10,12 +10,34 @@
 
 using namespace std;
 
-//Function Prototypes
-float computeDifferemce(float Hr,float HR,float Min,float MIN, char AM,char PM) {
-        if(PM=AM) returnHR-Hr<<MIN-Min<<
+//Function prototypes
+int computeDifference(int hr,int HR,int min,int MIN,char isAM){
+    float Hours = HR-hr;
+    float Mins  =MIN-min;
+    if(isAM=='y'||isAM=='Y') return Hours*60+Mins;
+    if(isAM=='n'||isAM=='N') return Hours*60+12*60+Mins;
+    return Hours*60 +Mins;
 }
 
-int main(int argc, const char * argv[]) {
+int main() {
+    int hr1,hr2,min1,min2;
+    char Equal;
+    
+    cout<<"Welcome to my time Machine!"<<endl;
+    cout<<"You may only go forward up to 25 hours."<<endl;
+    cout<<"Start time in hours : "<<endl;
+    cin>>hr1;
+    cout<<"End time in hours: "<<endl;
+    cin>>hr2;
+    cout<<"Start time in minutes : "<<endl;
+    cin>>min1;
+    cout<<"End time in minutes : "<<endl;
+    cin>>min2;
+    cout<<"Are they both of equal time periods?"<<endl;
+    cout<<"Example: are they both AM or are they both PM [Y/N] ?"<<endl;
+    cout<<"If incorrect input chosen, program will assume they are of same time period. "<<endl;
+    cin>>Equal;
+    cout<<"The amount travelled in minutes is : "<<computeDifference(hr1,hr2,min1,min2,Equal)<<endl;
     
     return 0;
 }
