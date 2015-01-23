13898623e8be1d264368aca1223359b1aa279945
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-22 20:52:48 -0800

    New Problem
    Started on new problem

---
+++ Hmwk/Assignment 4/Savitch_9thEd_chapter4_problem7/Savitch_9thEd_chapter4_problem7/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Savitch_9thEd_chapter4_problem7
+//
+//  Created by Guillermo  on 1/22/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+//Function Prototypes
+float computeDifferemce(float Hr,float HR,float Min,float MIN, char AM,char PM) {
+        if(PM=AM) returnHR-Hr<<MIN-Min<<
+}
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
