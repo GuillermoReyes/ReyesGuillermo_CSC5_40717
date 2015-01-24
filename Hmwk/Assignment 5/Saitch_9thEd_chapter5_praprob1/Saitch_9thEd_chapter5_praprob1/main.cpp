61e8db2a8b5d106c15102da5ec759ffc1f8eab9e
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-23 19:10:45 -0800

    New assignment 5 added

---
+++ Hmwk/Assignment 5/Saitch_9thEd_chapter5_praprob1/Saitch_9thEd_chapter5_praprob1/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Saitch_9thEd_chapter5_praprob1
+//
+//  Created by Guillermo  on 1/23/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+#inlude <cmath>
+
+using namespace std;
+//Funtion Prototype to alulate the average of 4 numbers
+float fourAverager(float a,float b, float c,  float d){
+    return (a+b+c+d)/4;
+}
+//Function that will alulate deviation
+float deviation(float s1, float s2, float, s3,float s4){
+    
+}
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
