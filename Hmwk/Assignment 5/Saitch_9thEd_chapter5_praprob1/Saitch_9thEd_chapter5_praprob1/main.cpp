22aa79cb346712dace59db066c2c24ef7c200749
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-24 19:21:09 -0800

    fixed a problem
    Assignment 5 nearly completed, need flow charts

--- Hmwk/Assignment 5/Saitch_9thEd_chapter5_praprob1/Saitch_9thEd_chapter5_praprob1/main.cpp
+++ Hmwk/Assignment 5/Saitch_9thEd_chapter5_praprob1/Saitch_9thEd_chapter5_praprob1/main.cpp
@@ -10,39 +10,36 @@
 #include <cmath>
 
 using namespace std;
-//Funtion Prototype to alulate the average of 4 numbers
-float fourAverage(float a,float b, float c,  float d){
-    return (a+b+c+d)/4;
-}
-//Function that will evalulate deviation
-float Dev(float s1, float s2, float, float s3, float s4){
-    float a= fourAverage( s1, s2,  s3,  s4);
-    return (pow(s1-a,2)+pow(s2-a,2)+pow(s3-a,2)+pow(s4-a,2))/4;
+
+//Funtion to calculate aerage and deviation.
+void average(float a,float b,float c,float d){
+    cout<<"First score:"<<endl;
+    cin>>a;
+    cout<<"Second score: "<<endl;
+    cin>>b;
+    cout<<"Third score: "<<endl;
+    cin>>c;
+    cout<<"Fourth score: "<<endl;
+    cin>>d;
+    float ave = (a+b+c+d)/4;
+    cout<<"The average is "<<ave<<endl;
+    float temp= pow(a-ave,2)+pow(b-ave,2)+pow(c-ave,2)+pow(d-ave,2);
+    float dev= temp/4;
+    cout<<"The standard deviation is:"<<dev<<endl;
+    
+    
 }
 
-   
 
 
 int main(int argc, const char * argv[]) {
-    float a,b,c,d; //Four sores to be used in calculation.
-    
-    char choose; //chooses wether to proceed with alculations or not
-    cout<<"This program will calculate the aerage, and average deviation. "<<endl;
+    char choose;
+    int a,b,c,d;
     do{
-    cout<<"first sore: "<<endl;
-    cin>>a;
-    cout<<"Second sore : "<<endl;
-    cin>>b;
-    cout<<"Third score : "<<endl;
-    cin>>c;
-    cout<<"Fourth score :"<<endl;
-    cin>>c;
-    float ave = fourAverage(a,b,c,d);
-    float temp = (pow(a-ave,2)+pow(b-ave,2)+pow(c-ave,2)+pow(d-ave,2))/4;
-    cout<<"The average is "<<fourAverage(a,b,c,d)<<endl;
-    cout<<"Average deviation is"<<sqrt(temp)<<endl;
-    cout<<"Would you like to try again? [Y/N]"<<endl;
-    cin>>choose;
-}while(choose=='y'||choose=='Y');
+        average( a, b, c, d);
+        cout<<"again? [Y/N]"<<endl;
+        cin>>choose;
+    
+    }while(choose=='y'||choose=='Y');
     return 0;
 }
---
+++ Hmwk/Assignment 5/Shooting game, Just for fun (ignore)/saivitch_9thEd_chapter5_problem6/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  saivitch_9thEd_chapter5_problem6
+//
+//  Created by Guillermo  on 1/24/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+//Function Prototypes
+//Function to calculate hit or miss
+void shoot(bool& targetAlive, float accuracy){
+    srand(time(NULL));
+    int a=rand()%2;
+    cout<<a<<endl;
+}
+//Funtion to begin a duel
+void startDuel()
+
+
+
+int main(int argc, const char * argv[]) {
+    bool charlieAlive;
+    shoot(charlieAlive,0);
+
+    return 0;
+}
