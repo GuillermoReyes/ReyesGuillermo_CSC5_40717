c3ea3cc6f3a0c16c781da5e41244c8de8815add8
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-28 11:05:14 -0800

    Signed-off-by: GuillermoReyes <greyes29@student.rcc.edu>

--- Project1/GamePrototype/main.cpp
+++ Project1/GamePrototype/main.cpp
@@ -8,13 +8,33 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <ctime>
 
 using namespace std;
+//Global Variable
+const float G= 9.8;  //acceleration due to gravity in m/sec^2
+
+//Function Prototypes
+//Calculates the assist of the boost packs
+void assist(float &a){
+    float mass  = 70.8;      //Weight of average adult in kilograms. source : google
+    float force = (-mass*G);    //force of gravity on a mass
+    float assist= force+a;
+    cout<<assist<<endl;
+}
 
 int main(int argc, char** argv) {
     char choose;                          //User decides when to escape game 
+    //the force that the boost pack apply 
+    float pack1 = 1000.0;
+    float pack2 = 10011.0;
+    float pack3 = 66000.0;
+    
+    // Basic skeleton of the program
     do {
-        cout<<"Hello"<<endl;
+        cout<<" You are flying ";
+        assist(pack1);
+        cout<<"Hello "<<endl;
         cin>>choose;
         
     }while(!(choose=='E'||choose=='e'));
