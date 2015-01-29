1cd3ea5422512105ad647426aec67ee5903a9fb8
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-29 08:45:47 -0800

    Signed-off-by: GuillermoReyes <greyes29@student.rcc.edu>

---
+++ Project1/MillionaireGame/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 29, 2015, 8:11 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
