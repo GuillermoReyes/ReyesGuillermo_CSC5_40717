14ac57aed5dcd8fdff89ce1b1acd82fb73d4ee8c
GuillermoReyes <greyes29@student.rcc.edu>
2015-01-29 09:14:06 -0800

    Signed-off-by: GuillermoReyes <greyes29@student.rcc.edu>

--- Project1/MillionaireGame/main.cpp
+++ Project1/MillionaireGame/main.cpp
@@ -6,14 +6,37 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <ctime>
+//Counts the  money gained from playing who wants to be a millionaire
+void moneyCount(int a ){
+    
+}
 
 using namespace std;
 
-/*
- * 
- */
 int main(int argc, char** argv) {
-
+    string leveloneQ[]={"What is the capital of California,"
+                        "Which president approved the FHA Reverse Mortgage?",
+                        "Which Inventor discovered Alternating current ?",
+                        "Macintosh and Linux are ____ based",
+                        "Europe is a country.",
+                        "Which Korean leader was featured in the recent film 'The Interview'",
+                        "___ pioneered mass production vehicles and made billions. ",
+                        "Who wrote the code for apple when it was a start-up",
+                        "Canada is North of ",
+                        "Harry Potter has a scar on his ___"};
+    char oneResp[]={"a. Sacramento b.Hawaii c.Stanford d.Topeka.",
+                         "a.Barrack Obama b.JFK c.Reagan d.FDR ",
+                         "a.Thomas Edision b.Tesla c.Jeff d.Fourier",
+                         "a.MS-DOS b.FAT32 c.UNIX d.Code ",
+                         "a.True b.False",
+                         "a.Kim Jong Un b.Kim il Un c.Kim Young Sam d.Roh Tae-Woo",
+                         "a.Warren Buffet b.Henry Ford c.Richard Branson d.Al sharpton",
+                         "a.Steve jobs b.Tim Cook c.Steve Wozniak d.Bill Gates",
+                         "a.United states b.Russia c. Japan d. Australia",
+                         "a. Arm  b.Leg c.Toe d.Forehead "};
+    char oneansw[] = {"a","c","b","c","b","a","b","c","a","d"};
     return 0;
 }
 
