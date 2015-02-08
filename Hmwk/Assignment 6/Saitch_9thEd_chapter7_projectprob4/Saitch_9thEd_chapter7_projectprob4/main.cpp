9b48fb48828e7a41a9ac6d0a0194dc05492e2ea3
GuillermoReyes <greyes29@student.rcc.edu>
2015-02-07 22:27:27 -0800

    new hw problem added

---
+++ Hmwk/Assignment 6/Saitch_9thEd_chapter7_projectprob4/Saitch_9thEd_chapter7_projectprob4/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  Saitch_9thEd_chapter7_projectprob4
+//
+//  Created by Guillermo  on 2/7/15.
+//  Copyright (c) 2015 Guillermo . All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+//Funtion prototype that fills an array and calulates standard deviation.
+void fillArray(int size){
+    size-=1;
+    float a[size];
+    for(int i=0;i<=size;i++){
+        int input;
+        cout<<"Enter the number in positon "<<i+1<<endl;
+        cin>>input;
+        input=a[i];
+        
+        
+    }//loses for loop
+    
+}
+
+int main(int argc, const char * argv[]) {
+      //Exection begins here
+    int choose;
+    cout<<"THis program will alculate the standard deviation of series of numbers"<<endl;
+    cout<<"Enter the size of array"<<endl;
+    cin>>choose;
+    fillArray(choose);
+    
+    return 0;
+}
