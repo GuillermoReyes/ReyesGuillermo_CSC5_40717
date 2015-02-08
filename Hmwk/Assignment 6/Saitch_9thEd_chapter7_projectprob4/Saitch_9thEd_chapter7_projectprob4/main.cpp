f09906d0dca27b0d903ada3126a6c8986da107a8
GuillermoReyes <greyes29@student.rcc.edu>
2015-02-08 12:15:45 -0800

    HW problem added

--- Hmwk/Assignment 6/Saitch_9thEd_chapter7_projectprob4/Saitch_9thEd_chapter7_projectprob4/main.cpp
+++ Hmwk/Assignment 6/Saitch_9thEd_chapter7_projectprob4/Saitch_9thEd_chapter7_projectprob4/main.cpp
@@ -7,33 +7,50 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
-//Funtion prototype that fills an array and calulates standard deviation.
-void fillArray(int size){
-    size-=1;
-    float a[size];
-    for(int i=0;i<=size;i++){
-        int input;
-        cout<<"Enter the number in positon "<<i+1<<endl;
-        cin>>input;
-        input=a[i];
-        
-        
-    }//loses for loop
-    for(int i=0;i<=size;i++){
-        a[i]+=0;
-    }
-    
-}
 
-int main(int argc, const char * argv[]) {
-      //Exection begins here
-    int choose;
-    cout<<"THis program will alculate the standard deviation of series of numbers"<<endl;
-    cout<<"Enter the size of array"<<endl;
-    cin>>choose;
-    fillArray(choose);
+//Function that will calculate standard deviation
+void stDev(int size){
     
-    return 0;
+    float a[size];
+    for(int i=0;i<size;i++){
+        float input;
+     
+        cout<<"Enter value for spot # "<<i+1<<endl;
+        cin>>input;
+        a[i]=input;
+        
+    }//closes for loop
+   
+    float sum=0;
+    for(int i=0;i<size;i++){
+        sum+=a[i];
+    }
+   float avg=sum/size;
+    cout<<avg<<endl;
+    float plus=0;
+    //loop that will calulate deviation individually and fill it in a new array
+    for(int i=0;i<size;i++){
+        float value=a[i];
+       
+    
+        float dev=pow(value-avg,2);
+        plus+=dev;
+        
+    }//closes for loope
+    cout<<plus<<endl;
+    float temp=plus/size;
+    cout<<" The standard deviation is "<<sqrt(temp)<<endl;
+    
+}//closes function
+
+//main execution begins here
+int main(){
+    float choose;
+    cout<<"Enter the size of array : ";
+    cin>>choose;
+    stDev(choose);
+    return true;
 }
